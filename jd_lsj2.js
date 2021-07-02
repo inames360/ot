771ce3cca888924fa6aeda31d324c0bdb21f9513
inames360 <71207880+inames360@users.noreply.github.com>
2021-07-02 11:57:21 +0800

    Update jd_lsj2.js

--- jd_lsj2.js
+++ jd_lsj2.js
@@ -11,8 +11,9 @@ export lsjdh="jdAward4" ##兑换牛奶
 [task_local]
 22 19 * * *
 [Script] 
-cron "10 10,20 * * *" 
-const $ = new Env('柠檬京东零食街exchange');
+cron "22 19 * * *" 
+*/
+const $ = new Env('柠檬京东零食街');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
